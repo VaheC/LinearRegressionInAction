f488057eb81cb683eec9d22890546333bd67ec8d
Vahe Charchyan <vcharchian@gmail.com>
2022-02-10 15:07:41 +0400

    app.py has been filled

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import pickle
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+model = pickle.load(open('regression_model.pickle', 'rb'))
+
+@app.route('/',methods=['GET'])
+def Home():
+    return render_template('index.html')
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        ptemp = float(request.form['ptemp'])
+        hdf = int(str(request.form['hdf']).split(':')[0].strip())
+
+        data = pd.DataFrame([ptemp, hdf]).T
+        data.columns = ['Process temperature [K]', 'HDF']
+        prediction = model.predict(data)
+
+        prediction_text = f'Predicted air temperature is {prediction:.2f} %.'
+        return render_template('index.html', prediction_text=prediction_text)
+    else:
+        return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
+        
