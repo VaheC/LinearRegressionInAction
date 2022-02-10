22cfdc9d4734a6ca3d34f1b6289af7e4cf149634
Vahe Charchyan <vcharchian@gmail.com>
2022-02-10 09:06:22 +0400

    packages have been added in model.py

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 # Importing necessary packages
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import logging
+import warnings
+warnings.filterwarnings('ignore')
