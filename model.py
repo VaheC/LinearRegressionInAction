d81b98b511dcdda324c6c7a07173c524b8d9a638
Vahe Charchyan <vcharchian@gmail.com>
2022-02-10 09:11:26 +0400

    Some new packages have been loaded in model.py

--- model.py
+++ model.py
@@ -5,6 +5,12 @@ import pickle
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 import logging
+import os
 import warnings
 warnings.filterwarnings('ignore')
+
+
+
